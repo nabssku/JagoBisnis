27a81dd9d9e62f9a3491a1b9ebcfbbe2671dc4a6
nabils24 <nabilsahsadacode@gmail.com>
2026-05-17 13:51:56 +0700

    feat: implement product image upload functionality and media retrieval endpoint in ProductController

--- backend/dist/src/product/product.controller.js
+++ backend/dist/src/product/product.controller.js
@@ -1,10 +1,43 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
@@ -22,6 +55,7 @@ const update_product_dto_1 = require("./dto/update-product.dto");
 const platform_express_1 = require("@nestjs/platform-express");
 const multer_1 = require("multer");
 const path_1 = require("path");
+const fs = __importStar(require("fs"));
 let ProductController = class ProductController {
     productService;
     constructor(productService) {
@@ -33,6 +67,26 @@ let ProductController = class ProductController {
         }
         const url = `http://localhost:3001/uploads/${file.filename}`;
         return { url };
+    }
+    getMedia() {
+        const uploadsDir = './uploads';
+        if (!fs.existsSync(uploadsDir)) {
+            return [];
+        }
+        const files = fs.readdirSync(uploadsDir);
+        const mediaUrls = files
+            .filter((file) => {
+            const filePath = (0, path_1.join)(uploadsDir, file);
+            const stat = fs.statSync(filePath);
+            return stat.isFile() && file.match(/\.(jpg|jpeg|png|gif|webp|svg)$/i);
+        })
+            .map((file) => {
+            return {
+                name: file,
+                url: `http://localhost:3001/uploads/${file}`,
+            };
+        });
+        return mediaUrls;
     }
     create(req, businessId, dto) {
         return this.productService.create(req.user.id, businessId, dto);
@@ -78,6 +132,13 @@ __decorate([
     __metadata("design:paramtypes", [Object]),
     __metadata("design:returntype", void 0)
 ], ProductController.prototype, "uploadFile", null);
+__decorate([
+    (0, common_1.Get)('media'),
+    (0, swagger_1.ApiOperation)({ summary: 'Get all uploaded media' }),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", []),
+    __metadata("design:returntype", void 0)
+], ProductController.prototype, "getMedia", null);
 __decorate([
     (0, common_1.Post)(),
     (0, swagger_1.ApiOperation)({ summary: 'Create a new product' }),
